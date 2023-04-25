4572dc603e6e7218685b9c2fe9fbfe4d890a34a1
wirix <88577048+wirix@users.noreply.github.com>
2023-04-25 23:35:44 +0700

    first commit

---
+++ layouts/Layouts.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Layouts = () => {
+  return (
+    <div>
+      <header>header</header>
+      <main>main</main>
+      <footer>footer</footer>
+    </div>
+  );
+};
